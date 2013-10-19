fcd7d5d821b336d04369bd73bc06663a4acb8ff9
piecioshka <piecioshka@gmail.com>
2013-10-19 13:41:20 +0200

    fix task 5

--- source/zad5/main.c
+++ source/zad5/main.c
@@ -6,7 +6,8 @@ void printFancyDate(struct tm * time) {
   int month = time->tm_mon;
   int year = time->tm_year + 1900;
   int hour = time->tm_hour;
-  printf("%i.%i.%i %i\n", day, month, year, hour);
+  char * tm_zone = time->tm_zone;
+  printf("%i.%i.%i %i %s\n", day, month, year, hour, tm_zone);
 }
 
 int main (int argc, char * argv[]) {
@@ -21,11 +22,10 @@ int main (int argc, char * argv[]) {
 
   /* update time for current not time zone */
   gmStruct = gmtime(&gmTime);
+  printFancyDate(gmStruct);
+
   /* update time for local time zone */
   localStruct = localtime(&localTime);
-
-  /* print dates */
-  printFancyDate(gmStruct);
   printFancyDate(localStruct);
   return 1;
 }
